b9137f9e5bbd4d9f610292084c3966fe72e8dddf
Drew Yang <drew.yang.dev@gmail.com>
2021-10-13 23:04:20 -0500

    update make pdf kill server logic

--- pdf-product2.sh
+++ pdf-product2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo 'Killing all Jekyll instances'
-kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
-clear
-
 echo "Building PDF-friendly HTML site for Product2 ...";
 # bundle exec jekyll serve --detach --config _config.yaml,pdfconfigs/config_product2_pdf.yaml;
 bundle exec jekyll serve --detach --config _config.yaml;
@@ -10,4 +6,8 @@ echo "done";
 
 echo "Building the PDF ...";
 prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o resume/drewyang_dev_resume.pdf;
+echo "done";
+
+echo 'Killing all Jekyll instances'
+kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 echo "done";
